bff1fa50a7531f11978009703b5d0633fd0080e4
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-06-08 12:17:35 +0800

    tick larva unworking

--- src/main/java/com/khjxiaogu/beecrasy/beehive/BeeHiveBaseComponent.java
+++ src/main/java/com/khjxiaogu/beecrasy/beehive/BeeHiveBaseComponent.java
@@ -33,8 +33,10 @@ import com.khjxiaogu.beecrasy.beehive.slot.StacksHiveSlot;
 import com.khjxiaogu.beecrasy.components.BeeHiveArgumentation;
 import com.khjxiaogu.beecrasy.components.BeehiveArgumenter;
 import com.khjxiaogu.beecrasy.components.GenomeComponent;
+import com.khjxiaogu.beecrasy.components.WorldCalendar;
 import com.khjxiaogu.beecrasy.genome.Genome;
 import com.khjxiaogu.beecrasy.genome.GenomeWorkHelper;
+import com.khjxiaogu.beecrasy.item.LarvaItem;
 import com.khjxiaogu.beecrasy.utils.ItemValidateHelper;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -45,6 +47,8 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.ValueInput;
@@ -656,6 +660,17 @@ public class BeeHiveBaseComponent implements ValueIOSerializable{
 		case REDSTONE:shouldWork=hasRedstone;break;
 		default:
 			break;
+		}
+		if(!hiveInfo.isWorking()) {
+			long secs=serverLevel.getServer().getDataStorage().computeIfAbsent(WorldCalendar.TYPE).getSeconds();
+			
+			for(ResourceStackHiveSlot slot:combSlot) {
+				ItemStack itemStack=slot.getItem();
+				if(LarvaItem.isExpired(itemStack,secs)) {
+					ItemStack ret=LarvaItem.getProduct(itemStack, serverLevel);
+					slot.setItem(ret);
+				}
+			}
 		}
 		if(beginingTicks>0) {
 			beginingTicks+=speed;
