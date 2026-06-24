1ae3915e25576bd0b347196d077e9372c6a4c766
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-06-24 20:44:49 +0800

    Update ArgumentationSlot indices in HiveMenu

--- src/main/java/com/khjxiaogu/beecrasy/menu/HiveMenu.java
+++ src/main/java/com/khjxiaogu/beecrasy/menu/HiveMenu.java
@@ -59,9 +59,9 @@ public class HiveMenu extends BeeHiveBaseMenu {
 		this.addSlot(new CombSlot(slots,slotModifier,11,76,74));
 		this.addSlot(new CombSlot(slots,slotModifier,14,97,69));
 		
-		this.addSlot(new ArgumentationSlot(slots,slotModifier,14,14,29));
-		this.addSlot(new ArgumentationSlot(slots,slotModifier,15,14,47));
-		this.addSlot(new ArgumentationSlot(slots,slotModifier,16,14,65));
+		this.addSlot(new ArgumentationSlot(slots,slotModifier,15,14,29));
+		this.addSlot(new ArgumentationSlot(slots,slotModifier,16,14,47));
+		this.addSlot(new ArgumentationSlot(slots,slotModifier,17,14,65));
 		this.addPlayerInventory(8, 103, 161);
 	}
 
