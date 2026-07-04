0df268d5518afab9fd11949f4458b3e38bdf26af
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-07-04 11:14:52 +0800

    clear tag conflict

--- src/datagen/java/com/khjxiaogu/beecrasy/BeecrasyBlockTagGenerator.java
+++ src/datagen/java/com/khjxiaogu/beecrasy/BeecrasyBlockTagGenerator.java
@@ -54,8 +54,8 @@ public class BeecrasyBlockTagGenerator extends BeecrasyTagGenerator<Block> {
 
 		TagAppender<ResourceKey<Block>, Block> net = tag(Tags.MINABLE_NET);
 		net.add(rk(BEE_NEST_SMALL)).add(rk(BEE_NEST_NASCENT)).add(rk(BEE_NEST_MEDIUM)).add(rk(BEE_NEST_BIG)).add(rk(NATURAL_HIVE));
-		tag(Tags.FLOWERS).addTag(BlockTags.FLOWERS);
-		tag(Genes.Alleles.WILD.getTag()).addTag(BlockTags.FLOWERS);
+		//tag(Tags.FLOWERS).addTag(BlockTags.FLOWERS);
+		//tag(Genes.Alleles.WILD.getTag()).addTag(BlockTags.FLOWERS);
 		tag(Tags.TO_BE_FLOWER).add(rk(Blocks.SHORT_DRY_GRASS),rk(Blocks.SHORT_GRASS),rk(Blocks.TALL_DRY_GRASS),rk(Blocks.TALL_GRASS),rk(Blocks.LARGE_FERN),rk(Blocks.FERN));
 		tag(Tags.MAILBOX).add(rk(BEE_NEST_SMALL)).add(rk(BEE_NEST_NASCENT)).add(rk(BEE_NEST_MEDIUM)).add(rk(BEE_NEST_BIG)).add(rk(NATURAL_HIVE));
 		tag(Tags.BEECITY_SPREADABLE).add(rk(EMPTY_COMB_BLOCK)).add(rk(HONEY_COMB_BLOCK));
